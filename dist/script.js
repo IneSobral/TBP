b405416adac23c4e71aa1eee304b3c163b8acbe7
ineSob <inesobral@icloud.com>
2023-09-18 09:13:31 +0100

    .

--- dist/script.js
+++ dist/script.js
@@ -53,9 +53,9 @@ document.addEventListener('DOMContentLoaded', function () {
 				}
 			} else {
 				if (!isPT) {
-					targetPage = currentPath.replace('/index.html', '/index-pt.html');
-				} else {
 					targetPage = currentPath.replace('/en/', '/pt/') + '-pt';
+				} else {
+					targetPage = currentPath.replace('/index.html', '/index-pt.html');
 				}
 			}
 
